20b99d8afb1233e2d216b990c97af5ce211e6c5a
wouttotti <wout_kamp@hotmail.com>
2015-05-22 12:30:03 +0200

    Beetje probleem
    IM GONNA CUT UR EYELIDS SO U CAN WATCH THE END

--- ICT4Events WebApplication/ICT4Events WebApplication/Global.asax.cs
+++ ICT4Events WebApplication/ICT4Events WebApplication/Global.asax.cs
@@ -13,10 +13,7 @@ namespace ICT4Events_WebApplication
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterOpenAuth();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
         }
 
         void Application_End(object sender, EventArgs e)
