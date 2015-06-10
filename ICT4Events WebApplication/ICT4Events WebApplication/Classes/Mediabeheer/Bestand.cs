717928df803a5a168ffe651f3138f92c33a6e409
HecticKaluha <s.verhoeve@student.fontys.nl>
2015-06-10 17:05:16 +0200

    FAY

---
+++ ICT4Events WebApplication/ICT4Events WebApplication/Classes/Mediabeheer/Bestand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICT4Events_WebApplication.Classes.Mediabeheer
+{
+    public class Bestand
+    {
+    }
+}
