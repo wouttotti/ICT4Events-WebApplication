29de8f5cc9dd24cf6fa6a6e205e38e7f74b89fc2
Robin Janssen <robinjanssen@gmail.com>
2015-06-12 13:24:40 +0200

    Reserveren Toegevoegd + Map convert to ASP

---
+++ ICT4Events WebApplication/ICT4Events WebApplication/Classes/ReserveringBeheer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICT4Events_WebApplication.Classes
+{
+    using System.Data;
+    using System.Windows.Forms;
+
+    public class ReserveringBeheer
+    {
+        private static Database database = new Database();
+
+        public bool Reserveren(string persoonID, DateTime aankomstDatum, DateTime vertrekDatum, string betaald)
+        {
+            try
+            {
+                string query =
+                    @"INSERT INTO RESERVERING(""persoon_id"",""datumStart"",""datumEinde"",""betaald"")VALUES('"
+                    + persoonID + "', TO_DATE('" + aankomstDatum + "','DD/MM/YYYY HH24:MI:SS'),TO_DATE('" + vertrekDatum
+                    + "','DD/MM/YYYY HH24:MI:SS'),'0')";
+                database.Insert(query);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public string VindReserveringNummer(string persoonID, DateTime aankomstDatum, DateTime vertrekDatum)
+        {
+            try
+            {
+                string query = @"SELECT id FROM Reservering WHERE ""persoon_id"" = " + persoonID
+                               + "AND \"datumStart\" = TO_DATE('" + aankomstDatum
+                               + "', 'DD/MM/YYYY HH24:MI:SS') AND \"datumEinde\" = TO_DATE('" + vertrekDatum
+                               + "', 'DD/MM/YYYY HH24:MI:SS')";
+                DataTable reserveringen = database.voerQueryUit(query);
+                string[] array = new string[1];
+                foreach (DataRow dr in reserveringen.Rows)
+                {
+                    array[0] = dr[0].ToString();
+                }
+                string reserveringNummer = array.GetValue(0).ToString();
+                return reserveringNummer;
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Reserveringsnummer kan niet worden gevonden.");
+                return null;
+            }
+        }
+
+        public bool KoppelKampeerplaats(string reserveringID, string kampeerplaatsnummer)
+        {
+            try
+            {
+                string queryInsert =
+                    @"INSERT INTO PLEK_RESERVERING(""plek_id"",""reservering_id"")VALUES('"
+                    + kampeerplaatsnummer + "','" + reserveringID + "')";
+                database.Insert(queryInsert);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public string VindKampeerplaatsID(int kampeerPlaatsnummer)
+        {
+            try
+            {
+                string query = @"SELECT id FROM Plek WHERE ""nummer"" = '" + kampeerPlaatsnummer + "'";
+                DataTable reserveringen = database.voerQueryUit(query);
+                string[] array = new string[1];
+                foreach (DataRow dr in reserveringen.Rows)
+                {
+                    array[0] = dr[0].ToString();
+                }
+                string kampeerplaatsID = array.GetValue(0).ToString();
+                return kampeerplaatsID;
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("KampeerplaatsID kan niet worden gevonden.");
+                return null;
+            }
+        }
+    }
+}
---
+++ ICT4Events WebApplication/ICT4Events WebApplication/WebForms/Map.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ICT4Events_WebApplication.WebForms
+{
+    using System.Windows.Forms;
+
+    public partial class Map : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
